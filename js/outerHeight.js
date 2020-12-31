a3598666b19f916c3cc0086ea0ae77b6bbb8f505
kaikaiwho <kaikaiwhooo@gmail.com>
2020-12-31 13:42:09 +0800

    Site updated: 2020-12-31 13:42:06

---
+++ js/outerHeight.js
@@ -0,0 +1,2 @@
+var outer = document.getElementById("outer"), header = document.getElementById("header"), footer = document.getElementById("footer");
+outer.style.minHeight=(window.innerHeight-header.offsetHeight-footer.offsetHeight)+"px";
